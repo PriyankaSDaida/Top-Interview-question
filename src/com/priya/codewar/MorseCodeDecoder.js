1adb6ae3d954cdb5551ed7d1d2d010a41390a103
PriyankaSDaida <priyankasudhakarreddy@gmail.com>
2025-01-15 14:26:50 -0800

    Create MorseCodeDecoder.js

---
+++ src/com/priya/codewar/MorseCodeDecoder.js
@@ -0,0 +1,18 @@
+/*
+voice and digital data communication channels, it still has its use in some applications around the world.
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example, the letter A is coded as ·−,
+letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive, traditionally capital
+letters are used. When the message is written in Morse code, a single space is used to separate the character codes
+and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+
+NOTE: Extra spaces before or after the code have no meaning and should be ignored.
+
+Your task is to implement a function that would take the morse code as input and return a decoded human-readable string.
+
+For example:
+MorseCodeDecoder.decode(".... . -.--   .--- ..- -.. .")
+//should return "HEY JUDE"
+
+All the test strings would contain valid Morse code, so you may skip checking for errors and exceptions.
+Good luck!
+*/
